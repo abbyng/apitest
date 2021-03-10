b0841bd77b0d5e6b69777161e4630adef7644cfd
abbyng <80264644+abbyng@users.noreply.github.com>
2021-03-10 23:56:48 +0100

    Update test_topics.py

--- test_topics.py
+++ test_topics.py
@@ -5,13 +5,19 @@ base_url='http://49.233.108.117:3000/api/v1'
 def test_topic_index_page():
     query_parmas = {
         "page":1,
-        "tab":"ask",
-        "limit":1,
+        "tab":"share",
+        "limit":2,
         "mdrender":"false"
     }
     r= requests.get(base_url+"/topics",params=query_parmas)
+    print(r.json())
     assert r.status_code == 200
     assert r.json()['success'] == True
 
+    data = r.json()["data"]
+    assert len(data) == query_parmas['limit']
+
+    for topic in data:
+        assert topic ['tab'] == query_parmas['tab']
 
 test_topic_index_page()
